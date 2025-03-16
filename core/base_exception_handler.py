7a8ad76bdad182091dd3e4c1efe6e9cb16784e10
M2-SYNDICATE <gptsemail.com@gmail.com>
2025-03-16 15:53:43 +0300

    fix clicable links for files

--- core/base_exception_handler.py
+++ core/base_exception_handler.py
@@ -1,6 +1,5 @@
 import traceback
 from rich import print as printr
-from rich import traceback as tb
 
 
 def StandardTextExceptionHandler(e):
@@ -9,7 +8,7 @@ def StandardTextExceptionHandler(e):
     line_number = last_frame.lineno
     path = last_frame.filename
 
-    message = f":warning: [red bold][ ERR ][/] В модуле [red bold link=vscode://{path}:{line_number} ]{last_frame.name}[/](<- кликабельно) возникла ошибка, смотри на [red bold]{line_number}[/] строке!"
+    message = f":warning: [red bold][ ERR ][/] В модуле [red bold link=vscode://file//{path}:{line_number} ]{last_frame.name}[/](<- кликабельно) возникла ошибка, смотри на [red bold]{line_number}[/] строке!"
 
     printr(message)
     printr("\n", *traceback.format_tb(e.__traceback__), f"{e}\n")
